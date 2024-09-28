0e9a67de5f4fd2bf0ce16bcf6bd2606d7c2ce9ab
ptk244 <94042348+ptk244@users.noreply.github.com>
2024-09-28 11:01:57 +0530

    ffs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndusspayFrontPageComponent } from './indusspay-front-page/indusspay-front-page.component';
 import { FooterComponent } from './footer/footer.component';
+import { OurStoryComponent } from './src/app/indusspay-front-page/our-story/our-story.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndusspayFrontPageComponent,
-    FooterComponent
+    FooterComponent,
+    OurStoryComponent
   ],
   imports: [
     BrowserModule,
