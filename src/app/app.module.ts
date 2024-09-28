9802206508ad0c9f57b438760daefa35bbb74cd8
kshitijashivadeMFS <kshitijars18@gmail.com>
2024-09-28 10:22:52 +0530

    footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndusspayFrontPageComponent } from './indusspay-front-page/indusspay-front-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndusspayFrontPageComponent
+    IndusspayFrontPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
